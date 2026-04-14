d9372f1c0f2ae4f86433e8ec2950010dcf4ada3e
Carlos Sánchez <tupijallegar@gmail.com>
2026-04-14 08:46:51 +0200

    Add next use case to test as comment to string calculator java

--- java/string-calculator/src/main/java/StringCalculator.java
+++ java/string-calculator/src/main/java/StringCalculator.java
@@ -2,7 +2,9 @@ import static java.lang.IO.println;
 
 // Ref: https://osherove.com/tdd-kata-1
 // TODO:
-//  - Allow the Add method to handle an unknown amount of numbers
+//  Allow the Add method to handle new lines between numbers (instead of commas).
+//      - the following input is ok: “1\n2,3” (will equal 6)
+//      - the following input is NOT ok: “1,\n” (not need to prove it - just clarifying)
 public class StringCalculator {
     public static int add(String numbers){
         String[] singleNumbers = numbers.split(",");
